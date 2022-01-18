a4e545848ce0945846e8455f60005a61ee45f8c8
Vincent DeStefano <VDeStefano@mhainc.com>
2022-01-18 16:08:39 -0500

    adding

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 3, 4, 8, 7, 9, 13 };
+            List<int> sampleNums = new List<int>();
+
+            sampleNums.AddRange(nums);
+
+            var AboveBelowRTS = new RTS();
+            Console.WriteLine(AboveBelowRTS.aboveBelow(sampleNums, 5).ToString());
+            Console.WriteLine(AboveBelowRTS.stringRotation("TestString", 3));
+
+        }
+    }
+}
---
+++ ConsoleApp1/RTS.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class RTS
+    {
+
+        public RTS() { 
+        
+        }
+        public object aboveBelow(List<int> unsortedListRTS, int compValue){
+
+            int above = 0;
+            int below = 0;
+ 
+            for(var i = 0; i< unsortedListRTS.Count; i++)
+            {
+                int valueInList = unsortedListRTS[i];
+                if(valueInList > compValue)
+                {
+                    above++;
+                }
+                if (valueInList < compValue)
+                {
+                    below ++;
+                }
+
+            }
+            dynamic aboveBelowRTS = new
+            {
+                above = above,
+                below = below
+            };
+            return aboveBelowRTS;
+        }
+
+
+        public string stringRotation(string randomString, int rightRotation)
+        {
+            string leftValue = randomString.Substring(randomString.Length - rightRotation);
+            string rightValue = randomString.Substring(0, randomString.Length - rightRotation);
+
+            string reOrderString = leftValue + rightValue;
+
+            return reOrderString;
+        }
+
+    }
+}
